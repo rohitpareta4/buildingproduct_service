86c0f67bd405ccc0b896b062ab7cca81db1ec658
rohitpareta4 <rockk7x@gmail.com>
2025-08-28 17:19:45 +0530

    solved problem

---
+++ buildingproduct_frontend/app/page.js
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+     <div className="justify-center font-bold items-center flex">
+      hello it's a service software
+     </div>
+  );
+}
